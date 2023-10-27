2d381444d6f77567f629ed93f313a0428696a180
Kenneth Stigen <kenneth.stigen@knowit.no>
2023-10-27 09:39:05 +0200

    Adding root-path from env

--- app/weather_api/config.py
+++ app/weather_api/config.py
@@ -5,6 +5,7 @@ class Settings(BaseSettings):
     env: str = "dev"
     server_port: int = 8080
     server_host: str = "0.0.0.0"
+    api_root_path: str = "/"
 
     met_api_url: str = "https://api.met.no/weatherapi"
 
--- app/weather_api/main.py
+++ app/weather_api/main.py
@@ -2,10 +2,15 @@ from fastapi import FastAPI
 
 from weather_api.routes import system_resources, weather_routes
 from weather_api.version import __version__
+from weather_api.config import settings
 
 
 def get_application() -> FastAPI:
-    api = FastAPI(title="Weather API", version=__version__, root_path_in_servers=False)
+    api = FastAPI(
+        title="Weather API",
+        version=__version__,
+        root_path=settings.api_root_path,
+    )
 
     api.include_router(system_resources.router)
     api.include_router(weather_routes.router)
