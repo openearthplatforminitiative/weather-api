cce0194e39ec95497725e91a215edba683db92bf
Kenneth Stigen <kenneth.stigen@knowit.no>
2024-06-18 11:51:43 +0200

    Fix redoc logo

--- weather_api/__main__.py
+++ weather_api/__main__.py
@@ -2,6 +2,7 @@ import pathlib
 
 from fastapi import FastAPI
 from fastapi.openapi.docs import get_redoc_html
+from fastapi.staticfiles import StaticFiles
 from prometheus_fastapi_instrumentator import Instrumentator
 
 from weather_api.config import settings
@@ -12,22 +13,30 @@ from weather_api.routes import system_resources, weather_routes
 def get_application() -> FastAPI:
     this_dir = pathlib.Path(__file__).parent
 
-    api = FastAPI(
-        root_path=settings.api_root_path,
+    app = FastAPI(
         redoc_url=None,
     )
+
+    api = FastAPI()
     api.include_router(weather_routes.router)
     api.include_router(system_resources.router)
 
-    api.openapi_schema = openapi.custom_openapi(api, this_dir / "example_code")
-    Instrumentator().instrument(api).expose(api)
-    return api
+    app.mount(
+        f"/static",
+        StaticFiles(directory=this_dir / "assets"),
+        name="static",
+    )
+    app.mount(f"{settings.api_root_path}", api)
+
+    app.openapi_schema = openapi.custom_openapi(app, this_dir / "example_code")
+    Instrumentator().instrument(app).expose(app)
+    return app
 
 
-app = get_application()
+application = get_application()
 
 
-@app.get("/redoc", include_in_schema=False)
+@application.get("/redoc", include_in_schema=False)
 def redoc():
     return get_redoc_html(
         openapi_url=f"{settings.api_root_path}/openapi.json",
@@ -40,7 +49,7 @@ if __name__ == "__main__":
     import uvicorn
 
     uvicorn.run(
-        "weather_api.__main__:app",
+        "weather_api.__main__:application",
         host=settings.server_bind_host,
         port=settings.server_bind_port,
     )
